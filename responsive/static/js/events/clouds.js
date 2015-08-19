79559cd7474c6f7a4ba8270f62cb545838f93e08
Rachel Fish <itsrachelfish@gmail.com>
2015-08-19 01:09:18 -0600

    More path testing

--- responsive/static/js/events/clouds.js
+++ responsive/static/js/events/clouds.js
@@ -4,21 +4,28 @@ function resize()
     var width = Math.max($('body').width(), $(window).width('outer'));
     height -= 40;
 
-    // Determine atmosphere size
+    // Determine atmosphere dimensions
     var atmosphere =
     {
-        background: $('.atmosphere').style('background-size').split(' ')};
-    }
+        size: $('.atmosphere').size(),
+        background: $('.atmosphere').style('background-size').split(' '),
+        position: $('.atmosphere').style('background-position').split(' ')
+    };
+
+    atmosphere.width = atmosphere.size.width * (parseInt(atmosphere.background[0]) / 100);
+    atmosphere.height = atmosphere.size.height * (parseInt(atmosphere.background[1]) / 100);
     
-    console.log(atmosphere);
+    $('.path, .cloud').style({height: atmosphere.height + 'px', width: atmosphere.width + 'px'});
 
-    $('.path').height();
 
-    $('.clouds, .cloud').style({height: height + 'px', width: '100%'});
+console.log(atmosphere.position);
+
+
+//    $('.clouds, .cloud').style({height: height + 'px', width: '100%'});
     
     $('.cloud').each(function()
     {
-        $(this).transform('translateY', height / 2.5 + 'px');
+  //      $(this).transform('translateY', height / 2.5 + 'px');
     });
 }
 
