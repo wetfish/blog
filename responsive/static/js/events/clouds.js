2a7f9d7a9f798446986cdd3fc04f863eb3d2bbf1
Rachel Fish <itsrachelfish@gmail.com>
2015-08-19 23:35:54 -0600

    Finished path experiments

--- responsive/static/js/events/clouds.js
+++ responsive/static/js/events/clouds.js
@@ -1,9 +1,5 @@
 function resize()
 {
-    var height = Math.max($('body').height(), $(window).height('outer'));
-    var width = Math.max($('body').width(), $(window).width('outer'));
-    height -= 40;
-
     // Determine atmosphere dimensions
     var atmosphere =
     {
@@ -13,26 +9,14 @@ function resize()
     };
 
     atmosphere.width = atmosphere.size.width * (parseInt(atmosphere.background[0]) / 100);
-    atmosphere.height = atmosphere.size.height * (parseInt(atmosphere.background[1]) / 100);
+    atmosphere.height = atmosphere.size.height * (parseInt(atmosphere.background[1]) / 100) - 5;
     
     $('.path, .cloud').style({height: atmosphere.height + 'px', width: atmosphere.width + 'px'});
-
-
-console.log(atmosphere.position);
-
-
-//    $('.clouds, .cloud').style({height: height + 'px', width: '100%'});
-    
-    $('.cloud').each(function()
-    {
-  //      $(this).transform('translateY', height / 2.5 + 'px');
-    });
 }
 
 $(document).ready(function()
 {
-    $('.cloud').style({display: 'block'});
-
+    $('.path, .cloud').style({display: 'block'});
     resize();
     
     $(window).on('resize load', function()
