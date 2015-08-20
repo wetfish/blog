cbd5d4573d8da2bb3f09cc2c504c74e58e64fe31
Rachel Fish <itsrachelfish@gmail.com>
2015-08-20 00:24:22 -0600

    Automatic cloud resizing

--- responsive/static/js/events/clouds.js
+++ responsive/static/js/events/clouds.js
@@ -12,6 +12,34 @@ function resize()
     atmosphere.height = atmosphere.size.height * (parseInt(atmosphere.background[1]) / 100) - 5;
     
     $('.path, .cloud').style({height: atmosphere.height + 'px', width: atmosphere.width + 'px'});
+
+    // Scale clouds based on atmosphere size
+    $('.cloud image').each(function()
+    {
+        var cloud =
+        {
+            width: this.width.baseVal.value,
+            height: this.height.baseVal.value
+        };
+        
+        if($(this).data('width') && $(this).data('height'))
+        {
+            cloud.width = $(this).data('width');
+            cloud.height = $(this).data('height');
+        }
+        else
+        {
+            $(this).data('width', cloud.width);
+            $(this).data('height', cloud.height);
+        }
+
+        var difference = $('body').height() / $(window).height() / 2;
+
+        $(this).attr('width', cloud.width * difference);
+        $(this).attr('height', cloud.height * difference);
+
+        console.log(atmosphere.height * 0.1);
+    });
 }
 
 $(document).ready(function()
